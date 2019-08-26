dcb2a8600a0c6721386e81f55d3d3fd155abad10
BridgeLabz <bridgelabz@BridgeLabzs-MacBook-Pro.local>
2019-08-26 16:01:33 +0530

    Binding Library Project added

---
+++ LibraryDemo/LibraryDemo/LibraryDemo.h
@@ -0,0 +1,19 @@
+//
+//  LibraryDemo.h
+//  LibraryDemo
+//
+//  Created by BridgeLabz on 20/08/19.
+//  Copyright © 2019 BridgeLabz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibraryDemo.
+FOUNDATION_EXPORT double LibraryDemoVersionNumber;
+
+//! Project version string for LibraryDemo.
+FOUNDATION_EXPORT const unsigned char LibraryDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibraryDemo/PublicHeader.h>
+
+
